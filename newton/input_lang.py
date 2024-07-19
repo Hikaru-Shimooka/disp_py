624af693c18c7c0a691545e2ae07d7479f6ed660
Hikaru-Shimooka <airplaneb787mrj@gmail.com>
2024-07-19 17:36:29 +0900

    revised

---
+++ newton/input_lang.py
@@ -0,0 +1,17 @@
+# parameter settings
+
+vj = 1  # thermal speed
+kj = 1  # debye wavenumber
+wpe = 1  # electron plasma frequency
+
+# k range
+k_min, k_max, k_num = 0, 1, 100
+
+# wr range
+wr_min, wr_max, wr_num = 0, 5, 500
+
+# wi range
+wi_min, wi_max, wi_num = -1, 1, 500
+
+# threshold value below which a value is treated as zero
+eps = 1e-2
